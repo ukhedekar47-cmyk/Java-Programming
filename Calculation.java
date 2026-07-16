f0919f8b3b7dc24619e44551902dd0021756edb1
ukhedekar47-cmyk <ukhedekar47@gmail.com>
2026-07-16 23:08:54 +0530

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,36 @@
+// EXP.2: Program on Classes, Objects, and Method Overloading
+// NAME: Aryan Khedekar
+// UIN: 251P107
+public class Calculation{
+
+    void add(int a){
+        System.out.println("A VALUE = "+a);
+
+    }
+
+    void add(int a, int b){
+        System.out.println("A VALUE = "+a);
+        System.out.println("B VALUE = "+b);
+
+    }
+    
+
+    void add(int a, int b, int c){
+        System.out.println("A VALUE = "+a);
+        System.out.println("B VALUE = "+b);
+        System.out.println("C VALUE = "+c);
+
+    }
+
+    
+}
+
+
+class Calculator{
+    public static void main(String args[]){
+        Calculation c1 = new Calculation();
+        c1.add(100);
+        c1.add(123, 456);
+        c1.add(700, 300, 600);
+    }
+}
